a633fa3d0ff64363bb6891df5da69856e6811349
mrbugracelik <mrbugracelik@gmail.com>
2018-04-22 21:42:33 +0300

    005-Array
    size 10 dan 15 yaptım

--- src/004-ReverseArray/src/com/company/Main.java
+++ src/004-ReverseArray/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args)
     {
 	    int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
-	    int i, k, temp=0, size = 10;
+	    int i, k, temp=0, size = 15;
 
 	    for (i = 0; i < size / 2; i++) {
             temp = a[i];
