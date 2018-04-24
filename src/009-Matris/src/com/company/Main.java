fc837005e4d59126a437c153af537cd760ea7f14
mrbugracelik <mrbugracelik@gmail.com>
2018-04-24 17:17:59 +0300

    009-Matris
    1.Sütüne 1-2-3-4
    2.Sütüne 1-2-3-4
    3.Sütüne 1-2-3-4
    yazdırdım.

---
+++ src/009-Matris/src/com/company/Main.java
@@ -0,0 +1,26 @@
+
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m[][] = new int [4][3];
+        int row = 4;
+        int col = 3;
+        int count = 0;
+        for (int i = 0; i < col; i++) {
+            for (int k = 0; k < row; k++) {
+                count = count + 1;
+                m[k][i] = count;
+            }
+            count = 0;
+        }
+        for (int i = 0; i < row; i++) {
+            for (int k = 0; k < col; k++)
+                System.out.print(m[i][k] + " ");
+            System.out.println();
+        }
+    }
+}
