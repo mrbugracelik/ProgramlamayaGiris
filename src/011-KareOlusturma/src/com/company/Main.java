19bf5199b5cecaee8467ba1153f01817cb951142
mrbugracelik <mrbugracelik@gmail.com>
2018-04-24 17:57:20 +0300

    011-KareOlusturma
    Kullanıcıdan 6 girdigi varsayılsın 6 yıldızdan oluşan bir kare dizayn ettik.

---
+++ src/011-KareOlusturma/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i;
+	int n, k;
+
+	for (i = 1; i <= 6; i++){
+	    if (i == 1)
+	        for(k=1; k <= 6; k++)
+                System.out.print("*");
+	    else if (i == 6)
+            for(k=1; k <= 6; k++)
+                System.out.print("*");
+	    else {
+            System.out.print("*");
+
+            for (k = 1; k <= 6 - 2; k++)
+                System.out.print(" ");
+
+            System.out.print("*");
+        }
+        System.out.println();
+	}
+    }
+}
