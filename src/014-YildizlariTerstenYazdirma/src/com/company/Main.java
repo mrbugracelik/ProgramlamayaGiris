b31fba37805bde0ed3cdd27ccc42c14527cf354b
mrbugracelik <mrbugracelik@gmail.com>
2018-04-24 19:31:19 +0300

    014-YildizlariTerstenYazma

---
+++ src/014-YildizlariTerstenYazdirma/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Kullanıcıdan 5 girildigi varsayilsin...
+        int i, n, k, j;
+
+        for  (i = 0; i < 5; i++) {
+            for (j = 1; j <=     i; j++)
+                System.out.print(" ");
+            for (k = 0; k < 5 - i; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
+    }
+}
