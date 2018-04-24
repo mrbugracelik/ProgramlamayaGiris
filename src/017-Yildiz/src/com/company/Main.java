bc64f30292ad56ebd5d8312ee5fd57827efe617c
mrbugracelik <mrbugracelik@gmail.com>
2018-04-24 20:05:03 +0300

    017-Yildiz

---
+++ src/017-Yildiz/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Kullanıcıdan 5 girildigi varsayilsin...
+        int i, n, k, j;
+
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
+
+
+    }
+}
