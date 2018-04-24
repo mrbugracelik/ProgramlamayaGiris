1c4bc2561dfa7b1268ef7aa2d69fd862661c2c7b
mrbugracelik <mrbugracelik@gmail.com>
2018-04-24 17:03:45 +0300

    006-Matris
    Matrise 1 den başlıyarak elaman ataması yaptık

---
+++ src/006-Matris/src/com/company/Main.java
@@ -0,0 +1,24 @@
+
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m[][] = new int [4][3];
+        int row = 4;
+        int col = 3;
+        int count = 0;
+        for (int i = 0; i < row; i++)
+            for ( int k = 0; k < col; k++){
+                count = count + 1;
+                m[i][k] =count;
+            }
+        for (int i = 0; i < row; i++) {
+            for (int k = 0; k < col; k++)
+                System.out.print(m[i][k] + " ");
+            System.out.println();
+        }
+    }
+}
