107753436aae07c000142737f49089ba36aceed7
mrbugracelik <mrbugracelik@gmail.com>
2018-04-25 11:18:24 +0300

    027-Dizi
    Dizinin son n adet elemaninın sondan başa dogru yazdiran program

---
+++ src/027-Dizi/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i, n, k, j;
+        int a[] = {4, 8, 3, 1, 18, 9 ,21, 20, 5, 17};
+        int size = 10;
+        System.out.print("Dizin sondan kaç elemanini istiyorsunuz:");
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+
+        for (i = size - 1; i >= size - n; i--)
+            System.out.println(a[i]);
+
+
+
+    }
+}
