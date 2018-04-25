3830c0b8a20b30cdaf5f74542b36abc533163720
mrbugracelik <mrbugracelik@gmail.com>
2018-04-25 09:39:57 +0300

    025-Dizi
    Dizinin ilk N adet elamanini ekrana yazdiran program

---
+++ src/025-Dizi/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a[] = {4, 8, 3, 1, 18, 9 ,21, 20, 5, 17};
+        int size = 10;
+        int sum = 0, avg = 0, count = 0, n, i, k;
+
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+
+
+        for (i = 0; i < n; i++)
+            System.out.println(a[i]);
+
+
+    }
+    }
+
