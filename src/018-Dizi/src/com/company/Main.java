542a888e08fae631b3afe3690556c5a80be4160b
mrbugracelik <38352311+mrbugracelik@users.noreply.github.com>
2018-04-25 08:40:14 +0300

    018-Dizi
    Dizinin ortalamasini bulan kod eklendi

--- src/018-Dizi/src/com/company/Main.java
+++ src/018-Dizi/src/com/company/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         int a[] = {4, 8, 3, 1, 18, 9 ,21, 20, 5, 17};
         int size = 10;
-        int sum = 0;
+        int sum = 0, avg;
 
         for (int i = 0; i < size; i++)
             sum = sum + a[i];
+        avg = sum / size;
+        System.out.println("Dizinin ortalamasi:" + avg);
         System.out.print("Dizinin toplami:" + sum);
 
         }
