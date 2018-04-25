3b9d1f98b9b6992743d5187aabbd82fe59242198
mrbugracelik <mrbugracelik@gmail.com>
2018-04-25 08:45:01 +0300

    019-Dizi
    Dizinin tek elemanlarini ekrana yazdiran program

---
+++ src/019-Dizi/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a[] = {4, 8, 3, 1, 18, 9 ,21, 20, 5, 17};
+        int size = 10;
+        int sum = 0, avg;
+
+        for (int i = 0; i < size; i++) //Burdaki for tüm dizinin elamanlarina ulaşmama yardimci oluyor..
+            if (a[i] % 2 != 0)
+                System.out.println("Dizin tek elemani:" + a[i]);
+
+        }
+    }
+
