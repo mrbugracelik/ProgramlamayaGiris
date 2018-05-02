d90d84f2e28b7c863b2a51be61a66fb7c6220c4a
mrbugracelik <mrbugracelik@gmail.com>
2018-05-02 11:43:16 +0300

    046-Matris
    5'e 5'lik bir matrisi birim matrise çeviren program...

---
+++ src/046-Matris/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int m[][] = new int[5][5];
+        int row = 5, col = 5, i, k;
+
+        for (i = 0; i < row; i++)
+                m[i][i] = 1;
+
+        for (i = 0; i < row; i++) {
+            for (k = 0; k < row; k++)
+                System.out.print(m[i][k]);
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
+
