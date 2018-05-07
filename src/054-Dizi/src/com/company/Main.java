bf7ef56e7b81207e30e36415113ce65dcf36d1ac
mrbugracelik <mrbugracelik@gmail.com>
2018-05-07 20:46:06 +0300

    054-Dizi
    Dizide ilk tek sayiyi bulan program...

---
+++ src/054-Dizi/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a[] = {4,8,3,1,18,9,21,20,5,17};
+	int i = 0;
+
+	while (a[i] % 2 == 0)
+        i = i + 1;
+        System.out.println(a[i]);
+
+
+
+    }
+}
