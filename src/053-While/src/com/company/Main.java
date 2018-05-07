ac851375dcc887dfea815e150d04fddc0de34167
mrbugracelik <mrbugracelik@gmail.com>
2018-05-07 20:04:17 +0300

    053-While
    While deyimiyle ekrana 1'den 10'a kadar yazan program...
    While döngüsünü çevrim sayisinin belli olmadigi durumlarda tercih edilir eğer çevrim sayisi belli ise for dönğüsü tercih edilir...

---
+++ src/053-While/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+      int i = 1;
+      while (i <= 10) {
+          System.out.print(i + " ");
+          i = i + 1;
+      }
+
+
+
+    }
+}
+
