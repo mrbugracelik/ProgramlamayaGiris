6a55d4b786d69e3aefd39d161a3303504922625e
mrbugracelik <mrbugracelik@gmail.com>
2018-05-08 22:39:59 +0300

    061-Dizi
    Sadece B ' de olan elemanlari ekrana yazdiran program...

---
+++ src/061-Dizi/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+class ScannerSinifi {
+
+	public static void main(String args[]){
+	int a[] = {5,5,3,7,8,5,7,7,7,3,8,9,8,9,8,8,15,5,8,4};
+	int b[] = {3,25,80,7,30};
+	int sizea = 20, sizeb = 5, n, flag, i, k;
+		for (k = 0; k < sizeb; k++){
+			flag = 0;
+			for (i = 0; i < sizea; i++)
+				if (a[i] == b[k])
+					flag = 1;
+			if (flag == 0)
+				System.out.println(b[k] + " - ilk dizide bulunmuyor...");
+		}
+	}
+}
